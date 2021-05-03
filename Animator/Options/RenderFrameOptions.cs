627bdd906511634e85e17509b3f7b484fd809505
Wojciech Sura <wojciechsura@gmail.com>
2021-05-03 13:44:41 +0200

    Fixed options, so that FrameIndex and TimeOffset cannot be entered both at once.

--- Animator/Options/RenderFrameOptions.cs
+++ Animator/Options/RenderFrameOptions.cs
@@ -10,10 +10,10 @@ namespace Animator.Options
     [Verb("render-frame", HelpText = "Renders single frame of the animation.")]
     public class RenderFrameOptions : BaseRenderOptions
     {
-        [Option(Default = null, HelpText = "Frame number", Required = true, SetName = "Offset")]
+        [Option(Default = null, HelpText = "Frame number", Required = true, SetName = "FrameIndex")]
         public int? FrameIndex { get; set; }
 
-        [Option(Default = null, HelpText = "Time offset", Required = true, SetName = "Offset")]
+        [Option(Default = null, HelpText = "Time offset", Required = true, SetName = "TimeOffset")]
         public TimeSpan? TimeOffset { get; set; }        
     }
 }
